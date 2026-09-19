9a7503865c0188d5f3972f3ebcfe22df8f728f62
benjsuk <dev@benjs.uk>
2026-09-20 00:49:25 +0100

    Refactor transaction handling: define Transaction type and implement initial transaction data structure

--- api/index.ts
+++ api/index.ts
@@ -1,23 +1,26 @@
-type Transaction = {
-    amount: number,
-    description: string,
-    date: Date,
-    category?: string,
-}
+import type { Transaction } from "./transactions"
 
-function getTransactions(){
-
-}
-
-function addTransaction(transaction: Transaction){
-
-}
-
-const testTransaction: Transaction = {
+let transactions: Array<Transaction> = [{
     amount: 5,
     description: "bacon",
-    date: new Date(),
+    date: new Date("2026-09-01"),
     category: "food"
-}
+}, {
+    amount: 7.99,
+    description: "KFC",
+    date: new Date("2026-09-01"),
+    category: "food"
+}, {
+    amount: 40,
+    description: "savings",
+    date: new Date("2026-09-02"),
+    category: "savings"
+}];
 
-console.log(testTransaction.amount)
+let daysTotal: Dict<number> = {};
+transactions.forEach((transaction: Transaction) => {
+    const day = transaction.date.toDateString();
+    daysTotal[day] = (daysTotal[day] ?? 0) + transaction.amount;
+})
+
+console.log(daysTotal)
---
+++ api/transactions.ts
@@ -0,0 +1,17 @@
+type Transaction = {
+    amount: number,
+    description: string,
+    date: Date,
+    category?: string,
+}
+
+function getTransactions(){
+
+}
+
+function addTransaction(transaction: Transaction){
+
+}
+
+export type {Transaction}
+export {getTransactions, addTransaction}
