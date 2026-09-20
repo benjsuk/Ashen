a216ad2b5d41f0ea53a6bffe3fa94569ffcd0618
benjsuk <benjsuk@users.noreply.github.com>
2026-09-20 00:07:43 +0000

    Prettified Code!

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,32 @@
 import type { Transaction } from "./transactions";
 
-let transactions: Array<Transaction> = [ {   amount: 350,   description: "bacon",   date: new Date("2026-09-01"),   category: "food", }, {   amount: 799,   description: "KFC",   date: new Date("2026-09-01"),   category: "food", }, {   amount: 4000,   description: "savings",   date: new Date("2026-09-02"),   category: "savings", },
+let transactions: Array<Transaction> = [
+  {
+    amount: 350,
+    description: "bacon",
+    date: new Date("2026-09-01"),
+    category: "food",
+  },
+  {
+    amount: 799,
+    description: "KFC",
+    date: new Date("2026-09-01"),
+    category: "food",
+  },
+  {
+    amount: 4000,
+    description: "savings",
+    date: new Date("2026-09-02"),
+    category: "savings",
+  },
 ];
 
 let daysTotal: Dict<number> = {};
-transactions.forEach((transaction: Transaction) => { const day = transaction.date.toDateString(); daysTotal[day] = (daysTotal[day] ?? 0) + transaction.amount;
+transactions.forEach((transaction: Transaction) => {
+  const day = transaction.date.toDateString();
+  daysTotal[day] = (daysTotal[day] ?? 0) + transaction.amount;
 });
 
-Object.entries(daysTotal).forEach(([day, total]) => { console.log(`On ${day}, £${(total || 0) / 100} was spent.`);
+Object.entries(daysTotal).forEach(([day, total]) => {
+  console.log(`On ${day}, £${(total || 0) / 100} was spent.`);
 });
