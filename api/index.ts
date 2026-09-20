c4972af99b5c254e9c24a6e17d5773fce51bcd0c
Ben <dev@benjs.uk>
2026-09-20 15:31:17 +0100

    Refactor error handling and logging in index.ts; remove phpMyAdmin service from compose.yml; add node-color-log dependency

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,13 @@
+import logger from "node-color-log";
 import type { Transaction } from "./transactions";
 import { newTransaction } from "./transactions";
 import { $ } from "bun";
+import "node-color-log"
 
 try {
   await $`cd db_server/ && sudo docker compose up -d`.quiet();
 } catch (e) {
-  console.log(e);
+  logger.error(e)
   process.exit();
 }
 
@@ -21,15 +23,15 @@ try {
   });
 
   Object.entries(daysTotal).forEach(([day, total]) => {
-    console.log(`On ${day}, £${(total || 0) / 100} was spent.`);
+    logger.color('black').debug(`On ${day}, £${(total || 0) / 100} was spent.`);
   });
 
-  console.log(transactions);
+  logger.color('black').debug(transactions);
 } finally {
   try{
   await $`cd db_server/ && sudo docker compose down`.quiet();
   }catch(e){
-    console.log(e)
-    console.log("DB Server May Not Have Closed")
+    logger.error(e)
+    logger.error("DB Server May Not Have Closed")
   }
 }
