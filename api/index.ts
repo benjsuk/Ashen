471916c2755e7f0833cb3382b6464431bd5eec11
Ben <dev@benjs.uk>
2026-09-22 01:41:58 +0100

    Transactions Ref

--- api/index.ts
+++ api/index.ts
@@ -50,6 +50,9 @@ const server = Bun.serve({
       );
       return new Response(JSON.stringify(dbResult[0]));
     },
+    "/transactions": async () => {
+      return new Response()
+    },
     /*"/test-transaction": async () => {
  util.debug("Received /test-transaction GET; Calling DB");
  util.debug("Getting Transactions...")
